da93141b333c0bd804aa72edcfba5b0a9183a993
Moon HyoJae <followmoonrise@gmail.com>
2020-11-15 16:18:03 +0900

    practice : CounterReactor 구현, 스토리보드

--- ReactorkitPractice/ReactorkitPractice/ViewController.swift
+++ ReactorkitPractice/ReactorkitPractice/ViewController.swift
@@ -6,10 +6,28 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, StoryboardView {
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    func bind(reactor: CounterViewReactor){
+        increase.rx.tap.map{Reactor.Action.increase}.bind(to: reactor.action).disposed(by: disposeBag)
+        decrease.rx.tap.map{Reactor.Action.decrease}.bind(to: reactor.action).disposed(by: disposeBag)
+        
+        reactor.state.map{$0.value}.distinctUntilChanged().map{"\($0)"}.bind(to: label.rx.text).disposed(by: disposeBag)
+    }
+    
+    @IBOutlet var increase:UIButton!
+    @IBOutlet var decrease:UIButton!
+    @IBOutlet var label:UILabel!
+    
 	override func viewDidLoad() {
+
+        
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
@@ -17,3 +35,43 @@ class ViewController: UIViewController {
 
 }
 
+class CounterViewReactor :Reactor{
+    enum Action {
+        case increase
+        case decrease
+        
+    }
+    
+    enum Mutation{
+        case increaseValue
+        case decreaseValue
+        
+    }
+    
+    struct State {
+        var value:Int = 0
+    }
+    
+    let initialState = State()
+    
+    func mutate(action: CounterViewReactor.Action) -> Observable<CounterViewReactor.Mutation> {
+        switch action {
+        case .increase:
+            return Observable.just(Mutation.increaseValue)
+        case .decrease :
+            return Observable.just(Mutation.decreaseValue)
+        }
+    }
+    
+    func reduce(state: CounterViewReactor.State
+                , mutation: CounterViewReactor.Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .increaseValue:
+            newState.value  += 1
+        case .decreaseValue:
+            newState.value -= 1
+        }
+        return newState
+    }
+}
